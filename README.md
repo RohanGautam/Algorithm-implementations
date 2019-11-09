7857718c7ead884c5ebb4c29cc91f4554dfaf7a2
RohanGautam <rohan1gautam@gmail.com>
2019-11-09 21:13:06 +0800

    added bfs tree!

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Python implementations for algorithms learnt in CZ2001-Algorithms in NTU Singapo
     2. Open address hashing (Linear probing and double hashing)
 * Searching - Sequential and binary search
 * Sorting - Insertion sort, Mergesort and QuickSort
+* Graphs:
+    - anytree used (`pip install anytree`) for visualising trees.
--- graphs/bfs/breadthFirstSearch.py
+++ graphs/bfs/breadthFirstSearch.py
@@ -1,4 +1,5 @@
 import queue
+from anytree import Node, RenderTree
 '''
 Implementation of BFS - Breadth first search
 using: ../graphImage/graph1.png
@@ -31,23 +32,41 @@ def viewConnections():
 def indexOfNode(letter):
     return ord(letter)-65  # for uppercase letters only
 
-# bfs tree not implemented
+# bfs tree not implemented. without the tree, it's just traversal
 def bfs(graph, startNode, visitNext):
     # mark startnode as visited
     visited[startNode] = True
     visitNext.put(startNode)
     print(f'putting startnode : {startNode}')
     # >> add startNode to tree <<
+    firstNode = Node(startNode)
+    secondLastLevelNode = firstNode
+    count=0
     while visitNext.empty() == False:
         currentNode = visitNext.get()
+        if count == 0 :  # if root node of tree, set it as the root, and keep track of it via firstNode
+            currentTreeNode = firstNode
+            count+=1
+        else :
+            for childOfSecondLastLevel in secondLastLevelNode.children:
+                # if the node appears in a level before, make that the current node instead of initialising a new one
+                if childOfSecondLastLevel.name == currentNode:
+                    currentTreeNode = childOfSecondLastLevel
+        secondLastLevelNode = currentTreeNode
+
         row = graph[indexOfNode(currentNode)]
         neighbours = [indexmap[i] for i in range(len(row)) if row[i] == 1]
         for node in neighbours:
             if(visited[node] == False):
                 visited[node] = True
                 visitNext.put(node)
-                print(f'visited, putting: {node}')
                 # >> add edge between node and currentNode to tree <<
+                Node(node, parent=currentTreeNode)
+                print(f'visited, putting: {node}, parent is {currentNode}')
+    return RenderTree(firstNode)
 
 # G,E are unreachable
-bfs(adjList, 'A', visitNext) # passing these so it's clear what it needs if global variables not present
+bfsTree = bfs(adjList, 'A', visitNext) # passing these so it's clear what it needs if global variables not present
+print('bfs tree :')
+for pre, fill, node in bfsTree:
+    print("%s%s" % (pre, node.name))
