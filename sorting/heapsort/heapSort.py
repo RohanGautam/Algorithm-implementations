cb277f1c6e04ea07fec92e91d5c61e6b3f3a0209
RohanGautam <rohan1gautam@gmail.com>
2019-11-13 16:17:59 +0800

    used  currentSize, removed unused helper functions

--- sorting/heapsort/heapSort.py
+++ sorting/heapsort/heapSort.py
@@ -1,43 +1,17 @@
 from anytree import Node, RenderTree
 class Heap:
-    array = []
 
     def __init__(self, array=list('YRPDFBKAC')):
         self.array = array
+        self.currentSize = len(array)
     
     def printHeap(self):
         nodes = [Node(x) for x in self.array]
         for i, x in enumerate(self.array[1:]):
             nodes[i+1] = Node(x, parent= nodes[(i)//2])
-        for pre, fill, node in RenderTree(nodes[0]):
+        for pre, _ , node in RenderTree(nodes[0]):
             print(f'{pre}{node.name}')
 
-    def eleAtPos(self, pos):
-        if pos in range(len(self.array)):
-            return self.array[pos-1]
-        else:
-            return -1
-
-    def posOfEle(self, ele):
-        return self.array.index(ele)+1
-
-    def parentOfEle(self, ele):
-        # these are if pos from 1 onwards
-        return self.eleAtPos(self.posOfEle(ele)//2)
-
-    def leftSubtree(self, ele):
-        return self.eleAtPos(2*self.posOfEle(ele))
-
-    def rightSubtree(self, ele):
-        return self.eleAtPos(2*self.posOfEle(ele)+1)
-
-    def isLeaf(self, ele):
-        return self.eleAtPos(2*self.posOfEle(ele)) == -1
-
-    def popRightmost(self):
-        last = self.array[-1]
-        self.array = self.array[:-1]
-        return last
 
     def fixHeap(self, k, fromDeleteMax = False): # H, k but here H is global
         '''
@@ -62,8 +36,8 @@ class Heap:
             left = 2*self.array.index(k)+1
             
         # finding position to put k in
-        while left < len(self.array):
-            if left+1 < len(self.array) and self.array[left] < self.array[left+1]:
+        while left < self.currentSize:
+            if left+1 < self.currentSize and self.array[left] < self.array[left+1]:
                 left+=1
             if k>self.array[left]:
                 break
@@ -78,7 +52,7 @@ class Heap:
         self.heapify(0)
 
     def heapify(self, heapIndex):
-        isLeaf = True if 2*(heapIndex+1)>len(self.array) else False
+        isLeaf = True if 2*(heapIndex+1)>self.currentSize else False
         if not isLeaf:
             self.heapify(2*heapIndex+1) # the left subtree
             self.heapify(2*heapIndex+2) # the right subtree
@@ -87,16 +61,16 @@ class Heap:
     def getMax(self):
         return self.array[0]
     def deleteMax(self):
-        smallest = self.array[-1] # copy last(smallest) element
-        self.array = self.array[:-1] # delete last element
+        smallest = self.array[:self.currentSize][-1] # copy last(smallest) element in current size
+        self.currentSize -= 1 # "delete" last element
         self.fixHeap(smallest, fromDeleteMax=True)
 
     def heapsort(self):
         self.constructHeap()
-        for i in range(len(self.array)-1, -1, -1):
+        for i in range(self.currentSize-1, -1, -1):
             curMax = self.getMax()
             self.deleteMax() # deletes max and fixes the heap
-            self.array.append(curMax)
+            self.array[i] = curMax
         print(self.array)
         self.printHeap()
 
@@ -107,14 +81,17 @@ class Heap:
 
 
 
-h= Heap(list('ABCDFKPRY'))
+h = Heap(list("QWERTYUIOP"))
 h.heapsort()
+
+## RANDOM TESTS FOR DEBUGGING##
+# h= Heap(list('ABDEFKPRY'))
 # h.printHeap()
+# h.fixHeap('C', fromDeleteMax=True)
+# h.printHeap()
+
 # h.constructHeap()
-# # h.fixHeap('B')
-# h.printHeap()
 
 # # example that heaps are not unique, the output below is also a heap
 # h2 = Heap()
 # h2.printHeap()
-
